010c3b0bc6651b21e9442c32f1e3d188689d1717
Patryk Gąsiorowski <31520969+patrykga@users.noreply.github.com>
2021-06-07 23:15:49 +0200

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "azurerm" {
-    # modify the values for the storage account
     storage_account_name = "patrykgaterraformstate"
     container_name       = "testingterraform"
     key                  = "terraform.tfstate"
