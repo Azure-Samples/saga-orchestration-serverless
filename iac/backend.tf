41cb811ca89e18da0ea81b306ae0d7759fd93104
Peeyush Pachaori <peeyush.pachaori@gmail.com>
2022-06-12 21:19:38 -0500

    something

--- iac/backend.tf
+++ iac/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     # modify the values for the storage account
     storage_account_name = "saga0612"
-    container_name       = "testingterraform"
+    container_name       = "testingterraform1"
     key                  = "terraform.tfstate"
   }
 }
